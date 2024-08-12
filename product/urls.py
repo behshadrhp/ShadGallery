26fa4b4ac3d7a129493da8af7fd8236a002af1e8
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-08-12 10:41:45 +0330

    :zap: create Product page

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from product import views
+
 
 urlpatterns = [
-    
+    path('art/', views.ProductView.as_view(), name="products"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from product.models import Product
+
+
+class ProductView(View):
+    """
+    This class is for rendering Product page.
+    --> return all product.
+    """
+
+    def get(self, request):
+        products = Product.objects.select_related("user").all()
+
+        context = {"products": products}
+        return render(request, "gallery/products.html", context)
