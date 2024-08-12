60bf42220860c1999635411ede183de88776ad77
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-08-12 09:55:49 +0330

    :tada: create Home app

--- core/settings/common.py
+++ core/settings/common.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Internal App
-
+    "home",
     # External App
     "debug_toolbar",
 ]
--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ import debug_toolbar
 urlpatterns = [
     # admin panel
     path("admin/", admin.site.urls),
+    path("", include("home.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
